70c2cab7ebac75db1a34b893735052ce50230d6b
KING <kelvinksd500@gmail.com>
2023-09-17 12:06:32 +0100

    images

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h1 className="nav-head">King</h1>
+
+      <section className="footer-sec">
+        <p className="footer-text">Projects</p>
+        <p className="footer-text">Menu</p>
+        <p className="footer-text">About</p>
+        <p className="footer-text">f</p>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,10 @@
 import React from "react";
+import back from "../images/3rd no back.png";
+import back from "../images/3rd no back.png";
+import back from "../images/3rd no back.png";
+import back from "../images/3rd no back.png";
+import back from "../images/3rd no back.png";
+import back from "../images/3rd no back.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import {
 //   // faHouse,
@@ -13,11 +19,27 @@ import React from "react";
 
 function Landing() {
   return (
-    <div>
-      <h1>
+    <div className="land">
+      <h1 className="land-head">
         Hi, I’m a software engineer and a content creator posting regularly
         about my daily life and projects.
       </h1>
+
+      <section>
+        <img className="back" src={back} alt=""></img>
+        <img className="back" src={back} alt=""></img>
+        <img className="back" src={back} alt=""></img>
+        <img className="back" src={back} alt=""></img>
+        <img className="back" src={back} alt=""></img>
+        <img className="back" src={back} alt=""></img>
+      </section>
+
+      <section>
+        <p>
+          Recently, I have been working on a side project called OneMenu. It’s a
+          menu bar app for the Mac.
+        </p>
+      </section>
     </div>
   );
 }
