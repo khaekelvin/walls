629d31ad7684754bfcfe7986faaa2e5375b74cf7
KING <kelvinksd500@gmail.com>
2023-09-20 00:07:44 +0100

    grid

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -22,17 +22,74 @@ function Landing() {
   return (
     <div className="land">
       <h1 className="land-head">
-        Hi, I’m a software developer and a content creator posting regularly
-        about my daily life and projects.
+        Hi, I’m a software developer and a <br /> content creator posting
+        regularly about <br /> my daily life and projects.
       </h1>
 
       <section className="land-sec">
-        <img className="back" src={back} alt=""></img>
+        <div className="web-design grid">
+          <span>
+            <h2>Web Design</h2>
+            <p>
+              As a skilled web designer, I have a keen eye for aesthetics and a
+              strong understanding of user experience. I specialize in creating
+              visually appealing and user-friendly websites that effectively
+              communicate my clients’ brand and message. I am proficient in
+              HTML, CSS, and JavaScript, allowing me to bring designs to life
+              with interactive elements and smooth animations.
+            </p>
+            <button>View Project</button>
+          </span>
+          <img className="back" src={back} alt=""></img>
+        </div>
+
+        <div className="app-development grid">
+          <img className="back" src={back} alt=""></img>
+          <span>
+            <h2>App Development</h2>
+            <p>
+              In the fast-paced world of mobile apps, I offer my expertise in
+              app development to create seamless and intuitive user experiences.
+              I have a strong foundation in mobile app development frameworks,
+              such as React Native and Flutter, allowing me to build
+              cross-platform apps efficiently
+            </p>
+            <button>View Project</button>
+          </span>
+        </div>
+
+        <div className="animation grid">
+          <span>
+            <h2>Animation</h2>
+            <p>
+              Animation brings ideas to life in a captivating and engaging way.
+              With my animation skills, I can add that extra touch of magic to
+              various projects.
+            </p>
+            <button>View Project</button>
+          </span>
+          <img className="back" src={back} alt=""></img>
+        </div>
+
+        <div className="logo-design grid">
+          <img className="back" src={back} alt=""></img>
+          <span>
+            <h2>Logo Design</h2>
+            <p>
+              Logo design is both an art and a science, and I excel in creating
+              memorable and impactful logos. I understand the importance of a
+              strong brand identity, and my designs reflect that understanding.
+            </p>
+            <button>View Project</button>
+          </span>
+        </div>
+
+        {/* <img className="back" src={back} alt=""></img>
         <img className="back" src={noback} alt=""></img>
         <img className="back" src={poback} alt=""></img>
         <img className="back" src={cyback} alt=""></img>
         <img className="back" src={artback} alt=""></img>
-        <img className="back" src={cenback} alt=""></img>
+        <img className="back" src={cenback} alt=""></img> */}
       </section>
 
       <section className="land-sec-text">
